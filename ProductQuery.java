5013ebecf45459bf1f59cb69088516dbae24ff9d
KamronCorreia <47647775+KamronCorreia@users.noreply.github.com>
2020-09-05 19:30:02 -0600

    Update ProductQuery.java

--- ProductQuery.java
+++ ProductQuery.java
@@ -16,4 +16,22 @@ public class ProductQuery extends QueryBuilder{
 		keys.put("supplier_id", "a1");
 		
 	}
+	public ProductQuery(String keyWanted, String productName, int quantity, 
+			    double wholeCost, double salePrice, String Supplier) {
+		
+		super(keyWanted);
+      
+   
+      		KEYHEADER = "product_id";
+      		TABLENAME = "Products";
+      		TOTALCOLUMNS = 5;
+      
+      		// puts inserted quantities from input into the product query constructor
+      		keys.put(KEYHEADER, productName);
+		keys.put("quantity", quantity);
+		keys.put("wholesale_cost", wholeCost);
+		keys.put("sale_price", salePrice);
+		keys.put("supplier_id", supplier);
+		
+	}
 }
