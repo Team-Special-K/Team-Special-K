ed094890e3090c804558cdbdc62ce6803271f8fd
KamronCorreia <47647775+KamronCorreia@users.noreply.github.com>
2020-09-08 15:02:25 -0600

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,36 +1,133 @@
-package master;
+
+
 
 import java.sql.*;
+import java.util.Scanner;
+import java.io.*;
 
 public class Test {
 
-	public static void main(String[] args) throws SQLException {
 
-		Db sears = new Db();
-		ProductQuery q = new ProductQuery("sku123");
-		
-		//create db if it doesnt exist 
-		ResultSet result = sears.sendSqlStatement(q.dbExist());//sears.sendSqlStatement(q.dbExist());
-		if(!result.next()){sears.sendSqlStatement(q.createDb());}
-		
-		 //select db after creation
-		sears.sendSqlStatement("USE " + q.DBNAME + ";");//add to querybuilder?
-		
-		//create table if it doesnt exist
-		result = sears.sendSqlStatement(q.tableExist());//sears.sendSqlStatement(q.dbExist());
-		if(!result.next()){sears.sendSqlStatement(q.createTable());}
-		
-        //add a new row
-        q.keys.put("product_id", "abcZZZ");
-		sears.sendSqlStatement(q.addRow());
-			
-		//get a row(s)
-		q.keyWanted = "abcZZZ";
-        ResultSet a = sears.sendSqlStatement(q.getRow()); //iterate resultset to get values of row
-       
-        //iterate results - row location
-        while(a.next()){System.out.println(a.getRow());}
-		
-	}
+   /*
+   *  In the Main method we will be calling the various classes and methods we have already built 
+   *  to be sure that they work properly
+   */
+   
+   public static void main(String[] args) throws SQLException {
+   
+      Db test = new Db();
+   
+      ProductQuery PQTest = new ProductQuery("TestSku");
+   
+      ResultSet testResult = dataBaseTest(test);
+      
+      // call method to add all lines from doc into database
+      
+      addEntries("inventory_team5.csv", test);
+      
+      
+      // call method to check a random line in the database
+      checkEntry("OHD8WMCL4QV2");
+   
+   }
+   
+   
+   
+   
+   
+   /*
+   *  This method takes in a database and checks if it exists
+   *  if it doesn't it exist, the method creates it
+   */
+     
+   public static ResultSet dataBaseTest(Db test) {
+   
+      
+      ResultSet result = test.sendSqlStatement(PQTest.dbExist());
+      
+      
+      if(!result.next()){test.sendSqlStatement(PQTest.createDb());}
+      
+      return result;
+      
+   
+   }
+
+
+
+   /*
+   *  This method pulls the information from a text file and places it into a product query file
+   *  which is then added to the database
+   *  @param Db test takes in a database to add the information to
+   *  @param fileName is the name of the document you are using to add information
+   */
+
+   public static void addEntries(String fileName, Db test) {
+      
+      // read in excel file
+      
+      File inputDataFile = new File(fileName);
+      Scanner inputFile = new Scanner(inputDataFile);
+      
+      // generate a product query to be used to add each thing to the table 
+      ProductQuery pq;
+      
+      // gets rid of header row from table
+      String useless = inputFile.nextLine();
+      
+      // as long as the file isnt empty keep looping through the input file
+      while(inputFile.hasNext()) {
+      
+         String line = inputFile.nextLine();
+         
+         // split the string at each delimiter and return an array of the lines entries
+         
+         String[] data = line.split("[,]", 0);
+         
+         String productID = data[0];
+         String quantity = data[1];
+         String wholesale = data[2];
+         String salePrice = data[3];
+         String supplier = data[4];
+         
+         // generate new productQuery with items in hashmap already
+         pq = new ProductQuery(productID, quantity, wholesale, salePrice, supplier);
+         
+         // adds a new row to the table for each new item 
+         test.sendSqlStatement(pq.addRow());         
+         
+            
+      } 
+   
+   }
+   
+   /*
+   *  This method checks the database for a given productID and then 
+   *  returns the information associated with it
+   *  @param productID takes in a string to check the database for
+   */
+   
+   public static void checkEntry(String productID) {
+      
+      ProductQuery item = new ProductQuery(productID);
+      
+      ResultSet a = test.sendSqlStatement(item.getRow());
+      
+      while(a.next()){System.out.println(a.getRow());}
+      
+   
+   }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
