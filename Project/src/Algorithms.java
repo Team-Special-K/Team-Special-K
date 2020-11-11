e1e1c951de894c1850bbe279cbe1e49317b0d1bf
cesar-monzon <cmonzon1@msudenver.edu>
2020-11-11 02:37:01 -0700

    Button events now display no results On invalid dates or ranges with no results the jpanel is cleared and no results is displayed.

--- Project/src/Algorithms.java
+++ Project/src/Algorithms.java
@@ -3,6 +3,8 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.SQLSyntaxErrorException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -16,17 +18,22 @@ public class Algorithms {
      * @param ordersTable a sql query resultSet of all orders
      * @return array of tuples <totalspent, email>
      */
-    public ArrayList<Tup<Double,String>> getUsersSpent(ResultSet ordersTable) throws NumberFormatException, SQLException {
+    public ArrayList<Tup<Double, String, LocalDateTime>> getUsersSpent(ArrayList<String[]> ordersTable) {
 
-        CopyOnWriteArrayList<Tup<Double,String>> sumTotal = new CopyOnWriteArrayList<Tup<Double,String>>();
-        while(ordersTable.next()){
-            
-            String email = ordersTable.getString(1);
-            Integer quant = Integer.parseInt(ordersTable.getString(2));
-            Double price = Double.parseDouble(ordersTable.getString(3));
+        CopyOnWriteArrayList<Tup<Double,String, LocalDateTime>> sumTotal = new CopyOnWriteArrayList<Tup<Double,String, LocalDateTime>>();
+
+        for (var row : ordersTable){
+
+            String email = row[0];
+            Integer quant = Integer.parseInt(row[1]);
+            Double price = Double.parseDouble(row[2]);
+
+
+            assert(row[3].length() < 11);
+            LocalDateTime time = LocalDateTime.parse(row[3] + "T00:00:00.000000000");
             Double total = quant * price;
 
-            var newline = new Tup<Double, String>(total,email);
+            var newline = new Tup<Double, String, LocalDateTime>(total,email,time);
             boolean found = false;
 
             for(int i = 0; i < sumTotal.size();i++) 
@@ -34,7 +41,7 @@ public class Algorithms {
                 if(email.equals(sumTotal.get(i).y)){
                     Double newAddition =  total + sumTotal.get(i).x;
                     sumTotal.remove(i);
-                    var reinsert = new Tup<Double, String>(newAddition, email);
+                    var reinsert = new Tup<Double, String, LocalDateTime>(newAddition, email, time);
                     sumTotal.add(i, reinsert);
                     found = true;
                     break;
@@ -42,7 +49,46 @@ public class Algorithms {
             }
             if(!found){sumTotal.add(newline);}
         }
-        var sumTotalArr = new ArrayList<Tup<Double, String>>();
+        var sumTotalArr = new ArrayList<Tup<Double, String, LocalDateTime>>();
+        sumTotalArr.addAll(sumTotal);
+        return sumTotalArr;
+    }
+
+    /*
+     * Calculates total spent by each user email
+     * 
+     * @param ordersTable a sql query resultSet of all orders
+     * @return array of tuples <totalspent, email>
+     */
+    public ArrayList<Tup<Double, String, LocalDateTime>> getUsersSpent(ResultSet ordersTable) throws NumberFormatException, SQLException {
+
+        CopyOnWriteArrayList<Tup<Double,String, LocalDateTime>> sumTotal = new CopyOnWriteArrayList<Tup<Double,String, LocalDateTime>>();
+        while(ordersTable.next()){
+            
+            String email = ordersTable.getString(1);
+            Integer quant = Integer.parseInt(ordersTable.getString(2));
+            Double price = Double.parseDouble(ordersTable.getString(3));
+            assert(ordersTable.getString(4).length() < 11);
+            LocalDateTime time = LocalDateTime.parse(ordersTable.getString(4) + "T00:00:00.000000000");
+            Double total = quant * price;
+
+            var newline = new Tup<Double, String, LocalDateTime>(total,email,time);
+            boolean found = false;
+
+            for(int i = 0; i < sumTotal.size();i++) 
+            {
+                if(email.equals(sumTotal.get(i).y)){
+                    Double newAddition =  total + sumTotal.get(i).x;
+                    sumTotal.remove(i);
+                    var reinsert = new Tup<Double, String, LocalDateTime>(newAddition, email, time);
+                    sumTotal.add(i, reinsert);
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){sumTotal.add(newline);}
+        }
+        var sumTotalArr = new ArrayList<Tup<Double, String, LocalDateTime>>();
         sumTotalArr.addAll(sumTotal);
         return sumTotalArr;
     }
@@ -55,11 +101,11 @@ public class Algorithms {
      * @param x the number of sorted items to return
      * @return sorted array of tuples <totalspent, email>
      */
-    public ArrayList<Tup<Double,String>> getSortedTuples(ArrayList<Tup<Double,String>> unsorted, int x) {
+    public ArrayList<Tup<Double,String,LocalDateTime>> getSortedTuples(ArrayList<Tup<Double,String,LocalDateTime>> unsorted, int x) {
 
-        ArrayList<Tup<Double,String>> sorted = new ArrayList <Tup<Double,String>>();
+        ArrayList<Tup<Double,String,LocalDateTime>> sorted = new ArrayList<Tup<Double,String,LocalDateTime>>();
 
-        for(Tup<Double, String> tuple: unsorted){
+        for(Tup<Double,String,LocalDateTime> tuple: unsorted){
 
             int totalItems = sorted.size();
             if(totalItems == 0){
@@ -69,12 +115,15 @@ public class Algorithms {
             else if(totalItems > x) 
                 totalItems = x;
 
+            boolean found = false;
             for(int i = 0; i < totalItems; i++){
                 if(tuple.x > sorted.get(i).x){
                     sorted.add(i, tuple);
+                    found = true;
                     break;
                 }
             }
+            if(!found){sorted.add(tuple);}
         }
         return sorted;
     }
@@ -106,6 +155,7 @@ public class Algorithms {
     /*
      * Get all items quantity & wholesalecost
      * 
+     * @param db the database handle
      * @return ResultSet of items in DB
      */
     public ResultSet getProducts(Db db) throws SQLException, SQLSyntaxErrorException {
@@ -116,10 +166,46 @@ public class Algorithms {
     /*
      * Get all orders details
      * 
+     * @param db the database handle
      * @return ResultSet of orders in DB
      */
     public ResultSet getOrders(Db db) throws SQLException, SQLSyntaxErrorException, FileNotFoundException {
         String tableName = "Orders";
         return db.sendSqlStatement("SELECT cust_email, product_id, product_quantity FROM " + tableName + ";");
+    }
+
+    /*
+     * Filters array of orders by a date range
+     * 
+     * @param start the object from the start date text field
+     * @param end the object from the end date text field
+     * @param rows the object returned from the SQL query to the db
+     * @return ArrayList of order columns
+     */
+    public static ArrayList<String[]> filterByDate(LocalDateTime start, LocalDateTime end, ResultSet rows) throws SQLException {
+
+        final long startEpoch = start.toEpochSecond(ZoneOffset.UTC);
+        final long endEpoch = end.toEpochSecond(ZoneOffset.UTC);
+        long orderEpoch;
+        
+        int numColumns = rows.getMetaData().getColumnCount();
+        var filtered = new ArrayList<String[]>();
+
+        while(rows.next()){
+            
+            assert(rows.getString(4).length() < 11);
+            LocalDateTime time = LocalDateTime.parse(rows.getString(4) + "T00:00:00.000000000");
+            orderEpoch = time.toEpochSecond(ZoneOffset.UTC);
+
+            if(orderEpoch < startEpoch || orderEpoch > endEpoch) continue;
+
+            var line = new String[numColumns];
+
+            for(int i = 0; i < numColumns; i++){
+                line[i] = rows.getString(i + 1);
+            }
+            filtered.add(line);
+        }
+        return filtered;
     }
 }
--- Project/src/ButtonReportAssets.java
+++ Project/src/ButtonReportAssets.java
@@ -31,24 +31,23 @@ public class ButtonReportAssets extends ButtonReport {
                               + "JOIN products ON orders.product_id = products.product_id;";
         ResultSet allOrders = db.sendSqlStatement(joinAssetQuery);
 
-        ArrayList<String[]> filteredOrders = null;
         String EOL = "T00:00:00.000000000";
-
         LocalDateTime dateStartNow = LocalDateTime.parse(dateStart.getText() + EOL);
         LocalDateTime dateEndNow = LocalDateTime.parse(dateEnd.getText() + EOL);
+
+        ArrayList<String[]> filteredOrders = null;
 
         try{
             filteredOrders = Algorithms.filterByDate(dateStartNow, dateEndNow, allOrders);
         } catch(SQLException e){
             e.printStackTrace();
-            System.exit(1);
         }
-        assert(filteredOrders != null);
 
-        ArrayList<Tup<Double,String, LocalDateTime>> results = algorithm.getUsersSpent(filteredOrders);
-
-        int size = results.size() < MAX_RESULTS ? results.size() : MAX_RESULTS;
-        result = algorithm.getSortedTuples(results, size);
+        if(filteredOrders != null & filteredOrders.size() > 0){
+            ArrayList<Tup<Double,String, LocalDateTime>> results = algorithm.getUsersSpent(filteredOrders);
+            int size = results.size() < MAX_RESULTS ? results.size() : MAX_RESULTS;
+            result = algorithm.getSortedTuples(results, size);
+        }
     }
 
     /*
@@ -57,12 +56,19 @@ public class ButtonReportAssets extends ButtonReport {
     @Override
     protected void displayResult() {
         outputArea.removeAll();
-        int size = result.size() < MAX_RESULTS ? result.size() : MAX_RESULTS;
-        for(var item : result.subList(0, size)){
-            JLabel line = new JLabel();
-            line.setFont(new Font("Helvetica", Font.PLAIN, 13));
-            line.setText(item.y + "\s\s$" + String.format("%.2f", item.x));
-            outputArea.add(line);
+
+        if(result == null || result.size() < 1){
+            outputArea.add(new JLabel("NO RESULTS"));
+        }
+        else{    
+            int size = result.size() < MAX_RESULTS ? result.size() : MAX_RESULTS;
+
+            for(var item : result.subList(0, size)){
+                JLabel line = new JLabel();
+                line.setFont(new Font("Helvetica", Font.PLAIN, 13));
+                line.setText(item.y + "\s\s$" + String.format("%.2f", item.x));
+                outputArea.add(line);
+            }
         }
         outputArea.revalidate();   
     }
